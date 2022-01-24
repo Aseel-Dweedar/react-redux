92fe47101c7b5d5a6d8f32a23ef48d4b92a7e5d9
aseel-dweedar <archaseel.1992@gmail.com>
2022-01-24 21:18:29 +0200

    navigation streams

--- streams/clint/src/components/App.js
+++ streams/clint/src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import Header from './Header';
 
 function App() {
-    return <div>APP</div>;
+    return (
+        <div className='ui container' >
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/stream/new" component={StreamCreate} />
+                    <Route path="/stream/edit" component={StreamEdit} />
+                    <Route path="/stream/delete" component={StreamDelete} />
+                    <Route path="/stream/show" component={StreamShow} />
+                </div>
+            </BrowserRouter>s
+        </div >
+    );
 }
 
 export default App;
