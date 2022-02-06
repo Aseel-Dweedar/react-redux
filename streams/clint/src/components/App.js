44f2263e93459a0d8db463a5bc6d43217e82fe98
aseel-dweedar <archaseel.1992@gmail.com>
2022-02-06 23:28:00 +0200

    implement streaming video

--- streams/clint/src/components/App.js
+++ streams/clint/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
@@ -18,11 +18,13 @@ function App() {
             <Router history={history}>
                 <div>
                     <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/stream/new" component={StreamCreate} />
-                    <Route path="/stream/edit/:id" component={StreamEdit} />
-                    <Route path="/stream/delete/:id" component={StreamDelete} />
-                    <Route path="/stream/show/:id" component={StreamShow} />
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/stream/new" exacts component={StreamCreate} />
+                        <Route path="/stream/edit/:id" exacts component={StreamEdit} />
+                        <Route path="/stream/delete/:id" exacts component={StreamDelete} />
+                        <Route path="/stream/:id" exacts component={StreamShow} />
+                    </Switch>
                 </div>
             </Router>
         </div >
--- streams/clint/src/components/streams/StreamList.js
+++ streams/clint/src/components/streams/StreamList.js
@@ -37,7 +37,7 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className='large middle aligned icon camera' />
                     <div className='content' >
-                        <Link to={`stream/show/${stream.id}`} className='header' >
+                        <Link to={`stream/${stream.id}`} className='header' >
                             {stream.title}
                         </Link>
                         <div className='description' >{stream.description}</div>
